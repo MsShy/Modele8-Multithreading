907313650488d37fa01eb304dc8aa5de57c44f26
MsShy <tanyat93@mail.ru>
2016-04-19 18:15:20 +0100

    com

--- src/main/java/homework/Util/ReaderFile.java
+++ src/main/java/homework/Util/ReaderFile.java
@@ -1,25 +1,31 @@
 package homework.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class ReaderFile {
+	private static final Logger log = LoggerFactory.getLogger(ReaderFile.class);
 
-	public static String read(String fileName) throws FileNotFoundException {
-		StringBuilder builder = new StringBuilder();
+	private ReaderFile() {
+	}
+
+	public static String read(String fileName) {
 		try (FileReader reader = new FileReader(fileName);
 		     BufferedReader bufferedReader = new BufferedReader(reader)) {
+			StringBuilder builder = new StringBuilder();
 			String output;
 			while ((output = bufferedReader.readLine()) != null) {
 				builder.append(output).append("\n");
 			}
-
+			return builder.toString();
 		} catch (IOException e) {
-			e.printStackTrace();
+			log.error(e.getMessage(), e);
 		}
-		return builder.toString();
+		return null;
 	}
 
 }
